ce2f1f4d4b122b020ab4ff3904966aeca34f45e3
Flatiron School <flatironschool@Flatirons-MacBook-Air.local>
2019-05-20 15:03:17 -0400

    lab progress

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,11 +1,19 @@
 class ShoppingCart:
     # write your code here
-    def __init__(self, emp_discount=None):
-      pass
+    def __init__(self, total=0, emp_discount=None, items =[]):
+      self.total = total
+      self.employee_discount = emp_discount
+      self.items = items
+    
     def add_item(self, name, price, quantity=1):
-       pass
+      self.items.append(name)
+      self.total += (price * quantity)
+      self.prices = []
+      for i in range(quantity):
+        self.prices.append(price)
+    
     def mean_item_price(self):
-       pass
+       return self.total/len(self.prices)
 
     def median_item_price(self):
         pass
